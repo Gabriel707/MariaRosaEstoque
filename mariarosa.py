be17005ac197a875eb86a03bd2f3f05ccbd12f1c
Gabriel Araujo <gabrielsant95@gmail.com>
2024-10-20 17:07:03 -0300

    Maria Rosa controle de estoque - primeiro commit da app

---
+++ mariarosa.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect
+
+class Macaquinho:
+    def __init__(self, nome, cor, valor):
+        self.nome = nome
+        self.cor = cor
+        self.preco = valor
+
+macaquinho1 = Macaquinho('Macaquinho Raposa', 'Amarelo', 44.99)
+macaquinho2 = Macaquinho('Macaquinho Bambi', "Cinza estampado", 44.99)
+macaquinho3 = Macaquinho('Macaquinho Ursinho Polar', 'Azul Estampado', 44.99)
+lista = [macaquinho1, macaquinho2, macaquinho3]
+
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Maria Rosa Estoque', produtos=lista)
+
+@app.route('/novoproduto')
+def novoproduto():
+    return render_template('novoproduto.html', titulo='Novo Produto')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    cor = request.form['cor']
+    valor = request.form['valor']
+    macaquinho4 = Macaquinho(nome, cor, valor)
+    lista.append(macaquinho4)
+    return redirect('/')
+
+app.run(debug=True)
